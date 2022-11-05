924fa26cd708bf2cfe4b07ed251f3036e7918026
nBenton2000 <117543543+nBenton2000@users.noreply.github.com>
2022-11-05 16:55:00 -0400

    Initial Commit
    the initial commit of the file.

---
+++ communication.py
@@ -0,0 +1,52 @@
+
+
+import socket
+import sys
+
+PORT = 65432  # The port used by the server
+VM_IP = "192.168.1.15"
+
+def send():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.settimeout(30.0)#Time out of 30 seconds if not received
+        s.connect((SERVER, PORT))
+        s.settimeout(None)#Always set timeout to none before sending.
+        s.sendall(b"Hello, world")
+        data = s.recv(1024)
+
+def receive():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((ALL_IP, PORT)) #Inner brackets define a tuple
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            exit = False
+            while not exit:
+                data = conn.recv(1024)
+                if not data:
+                    exit = True
+                else:
+                    print(data.decode(),end="#")
+                conn.sendall(data)
+
+def exit():
+    print("Goodbye!")
+    sys.exit()
+
+def menuOption():
+    menuDisplay()
+    option = input("Enter option: ")
+    if option == "0":
+        exit()
+    elif option == "1":
+        send()
+    elif option == "2":
+        receive()
+    else:
+        print("Error, invalid input")
+
+def menuDisplay():
+    print("===The Python Communicator===\n1) Send Message\n" +
+     "2) Receive Message\n3) Exit")
+
