58fb3f4e2781c02640eedc8a6b3683253ef4d9d9
evanescobedo1 <rockridge1779@gmail.com>
2022-11-06 21:22:02 -0500

    Changes for formatting

--- communication.py
+++ communication.py
@@ -2,6 +2,9 @@
 
 import socket
 import sys
+import ipaddress
+
+
 
 PORT = 65432  # The port used by the server
 HOST = "192.168.1.15"
@@ -13,10 +16,10 @@ BINGUS_GUEST = "192.168.122.46"
 
 def send():
     message = input("Enter Message (max 4096 characters): ")
-    ip = input("Enter Recepient IP: ")
- 
+    ip = ipCheck()
+    
     addr = ((ip, PORT))
-    print(f"Startomg connection to {addr}")
+    print(f"Starting connection to {addr}")
     socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #socket1.setblocking(True)
     #socket1.connect(addr)
@@ -25,12 +28,17 @@ def send():
         socket1.settimeout(30.0)#Time out of 30 seconds if not received
         socket1.connect((ip, PORT))
         socket1.settimeout(None)#Always set timeout to none before sending.
+        print("Sending")
         socket1.sendall(bytes(message, 'utf-8'))
         data = socket1.recv(1024)
+        print("Message sent successfully.")
+        socket1.recv(1024)
+        print(data.decode(), end="\n")
 
 def receive():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("192.168.122.138", PORT)) #Inner brackets define a tuple CHANGE HOSTNAME TO THIS MACHINES NAME
+        print("Waiting for message on port 65432")
         s.listen()
         conn, addr = s.accept()
         with conn:
@@ -41,8 +49,11 @@ def receive():
                 if not data:
                     exit = True
                 else:
-                    print(data.decode(),end="#")
-                conn.sendall(data)
+                    print("Message:")
+                    print(data.decode(),end="\n")
+                    print("End of message")
+
+                conn.sendall("#<<END>>#")
 
 def exit():
     print("Goodbye!")
@@ -66,6 +77,21 @@ def menuDisplay():
      "1) Send Message\n" +
      "2) Receive Message\n" + 
      "0) Exit")
+
+##Will handle getting the ip address formatted right
+def ipCheck():
+    keepgoing = True
+    ip = ""
+    while(keepgoing):
+        ip = input("Enter Recipient IP:")
+        try:
+            ip_object = ipaddress.ip_address(ip)
+            keepgoing = False
+        except ValueError:
+            print("Error not a valid IPv4 Address")
+
+    return ip
+
 
 if __name__ == '__main__':
     menuOption()
