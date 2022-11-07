0ab8c21f21795554f16eaaf837a6c902e123c7b6
Evanescobedo1 <rockridge1779@gmail.com>
2022-11-07 14:58:17 -0500

    Documentation and PDF added

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 # cs325-project2
 Sends and Recieves messages between 2 computers
-
+using the java socket api
 
 
 #How to run
+#Prelimiary
+1) ensure on both computers the port is open and the box has been edited 
+in communication.py
 
+#To run
+1) on the server computer run: python3 communication.py in the parent dir
+of communication.py
+2) at the start up menu select the second option to recieve 
+3) on the client computer run: python3 communication.py in the parent dir
+of communication.py
+4) on the start up menu select the first option to send a message
 
 #Known Bugs:
--- communication.py
+++ communication.py
@@ -1,18 +1,22 @@
-
+##Communication.py
+#@Authors Nicolas Benton and Evan Escobedo
+#Allows for the communication between two computers accross a
+#network. 
 
 import socket
 import sys
 import ipaddress
 
 
-
+######################################################################
+#EDIT HERE FOR YOUR SERVER
 PORT = 65432  # The port used by the server
-HOST = "192.168.1.15"
+SERVER_IP = "192.168.122.138" # The ip address used by the server
+######################################################################
 
-DINGUS_SERVER = "192.168.122.200"
-BINGUS_GUEST = "192.168.122.46"
-
-
+#This function handles everything for the user performing the client side
+#operations.  The user will specifiy an ip address to communicate with
+#and a message to send to the server
 
 def send():
     message = input("Enter Message (max 4096 characters): ")
@@ -35,9 +39,12 @@ def send():
         socket1.recv(1024)
         print(data.decode(), end="\n")
 
+#This function handles the server side tasks.  The method will wait for any input on 
+# desired port.  Right now the bound ip address is hard coded in the config above with
+# ip of the host and port desired to be used specified by the user
 def receive():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("192.168.122.138", PORT)) #Inner brackets define a tuple CHANGE HOSTNAME TO THIS MACHINES NAME
+        s.bind((SERVER_IP, PORT)) #Inner brackets define a tuple CHANGE HOSTNAME TO THIS MACHINES NAME
         print("Waiting for message on port 65432")
         s.listen()
         conn, addr = s.accept()
@@ -56,30 +63,35 @@ def receive():
                     exit = True
                     conn.close()
 
+#Closes the program with a nice message
 def exit():
     print("Goodbye!")
     sys.exit()
 
+
+#Handles the menu for user input. 
 def menuOption():
     menuDisplay()
     option = input("Enter option: ")
-    if option == "0":
+    if option == "0": #Quit option 
         exit()
-    elif option == "1":
+    elif option == "1": #Client Side Option
         send()
-    elif option == "2":
+    elif option == "2": #Server Side Option
         receive()
-    else:
+    else: #Invalid input
         print("Error, invalid input")
-        menuOption()
+        menuOption() #Recursive call to loop
 
+#Prints a string containing reference numbers for various user functions
 def menuDisplay():
     print("===The Python Communicator===\n" +
      "1) Send Message\n" +
      "2) Receive Message\n" + 
      "0) Exit")
 
-##Will handle getting the ip address formatted right
+##Will handle getting the ip address formatted right.  Will continually prompt
+#the user should the ip address be malformed.
 def ipCheck():
     keepgoing = True
     ip = ""
@@ -93,6 +105,6 @@ def ipCheck():
 
     return ip
 
-
+#Call to start the program
 if __name__ == '__main__':
     menuOption()
